8f7f8aa263b71cb7937ac56049b08c7e4ee09c46
Aisudoragon <krybex101@gmail.com>
2023-06-11 19:39:33 +0200

    Wykonanie ćwiczenia 5. lab 2.

--- src/main/java/CGlab/Renderer.java
+++ src/main/java/CGlab/Renderer.java
@@ -72,6 +72,32 @@ public class Renderer {
 
     public void drawLineBresenhamInt(int x0, int y0, int x1, int y1) {
         // TODO: zaimplementuj
+
+        int white = 255 | (255 << 8) | (255 << 16) | (255 << 24);
+
+        int dx = Math.abs(x1 - x0);
+        int sx = x0 < x1 ? 1 : -1;
+        int dy = -Math.abs(y1 - y0);
+        int sy = y0 < y1 ? 1 : -1;
+        int err = dx + dy;
+
+        int e2;
+
+        while (true) {
+            drawPoint(x0, y0);
+            if (x0 == x1 && y0 == y1) break;
+            e2 = 2 * err;
+            if (e2 >= dy) {
+                if (x0 == x1) break;
+                err += dy;
+                x0 += sx;
+            }
+            if (e2 <= dx) {
+                if (y0 == y1) break;
+                err += dx;
+                y0 += sy;
+            }
+        }
     }
 
     public void save() throws IOException {
